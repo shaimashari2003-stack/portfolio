db99b434279655207f0017e370b42f5c602d23d2
shaimashari2003-stack <shaimashari2003@gmail.com>
2025-09-10 17:08:05 +0530

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-function sendMail() {
-    var params = {
-        name: document.getElementById("name").value,
-        email: document.getElementById("email").value,
-        message: document.getElementById("message").value,
-    };
-}
